83d48363e6f4830a82807ff81e0856df199abf14
João Vitor Rodrigues <155745191+RodriguesJV@users.noreply.github.com>
2025-05-22 21:49:47 -0300

    Add files via upload

---
+++ Lógica de programação/atividade4/ex001.js
@@ -0,0 +1,34 @@
+/*Nome: João Vitor Rodrigues dos Santos
+Matricula: 25114290126 */
+
+//1. Escolha uma tema. Crie 3 objetos relativos a este tema e que contenha 4 atributos.
+//Construa os objetos usando a foram literal.
+
+let artilheiro1 ={
+    nome: "Yuri Alberto",
+    posicao: "Centroavante",
+    clube:"Corinthians",
+    perna_boa: 'direita'
+}
+
+
+let artilheiro2 ={
+    nome: "Pedro",
+    posicao: "Centroavante",
+    clube:"Flamengo",
+    pais: 'Brasil'
+}
+
+
+let artilheiro3 ={
+    nome: "Yamal",
+    posicao: "Ponta esquerda",
+    clube:"Barcelona",
+    pais : "Espanha"
+}
+
+console.log(artilheiro1);
+console.log(artilheiro2);
+console.log(artilheiro3);
+
+
---
+++ Lógica de programação/atividade4/ex002.js
@@ -0,0 +1,75 @@
+//Nome: João Vitor Rodrigues dos Santos
+//Matricula: 25114290126 
+
+//2. Crie uma função construtora relativa à mesma classe (tema) que você utilizou na questão 1.
+function artilheiro(nome,posicao,clube,pais){
+    this.nome= nome;
+    this.posicao = posicao;
+    this.clube = clube;
+    this.pais = pais;
+    
+}
+
+console.log(artilheiro)
+
+//3. Crie 3 instâncias(objetos) da classe definida na questão 2.
+const artilheiros1 = new artilheiro ('Raphinha','Ponta direita','Barcelona','Brasil');
+const artilheiros2 = new artilheiro ('Belligham','Meio- campo','Real Madrid','Inglaterra');
+const artilheiros3 = new artilheiro ('Messi','Ponta direita','InterMiami','Argentino');
+
+//4. Busque todas as chaves dos objetos definidos em 3).
+
+console.log(Object.keys(artilheiros1));
+console.log(Object.keys(artilheiros2));
+console.log(Object.keys(artilheiros3));
+
+//5. Busque todos os valores dos objetos definidos em 3).
+console.log('-'.repeat(90));
+console.log(Object.values(artilheiros1));
+console.log(Object.values(artilheiros2));
+console.log(Object.values(artilheiros3));
+
+//6. Puxe todos os pares chaves-valores dos 3 objetos definidos.
+console.log('-'.repeat(90));
+console.log(artilheiros1);
+console.log(artilheiros2);
+console.log(artilheiros3);
+
+//7 Aplique for loop para buscar todas chaves para cada um dos objetos definidos.
+console.log('-'.repeat(90));
+for (const goat in artilheiros1){
+     console.log(goat)
+}
+
+console.log('-'.repeat(90));
+
+for (const goat in artilheiros2){
+     console.log(goat)
+} 
+console.log('-'.repeat(90));
+
+for (const goat in artilheiros3){
+     console.log(goat)
+}
+
+//8 Aplique for loop para buscar todos valores para cada um dos objetos definidos.
+console.log('-'.repeat(90));
+for (const goat in artilheiros1){
+     console.log(artilheiros1[goat])
+}
+console.log('-'.repeat(90));
+for (const goat in artilheiros2){
+     console.log(artilheiros2[goat])
+}
+console.log('-'.repeat(90));
+for (const goat in artilheiros3){
+     console.log(artilheiros3[goat])
+} 
+console.log('-'.repeat(90));
+
+//9 Crie um array que armazene todos os 3 objetos que você criou.
+let goat = [];
+goat.push(artilheiros1, artilheiros2, artilheiros3);
+console.log(goat);
+
+//10 Adicione um elemento ao final do array criado e outro ao início.
