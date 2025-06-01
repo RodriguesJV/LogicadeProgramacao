91a844f3b3c8aa52434a4870b05420f8eb26929e
João Vitor Rodrigues <155745191+RodriguesJV@users.noreply.github.com>
2025-05-31 22:12:17 -0300

    Add files via upload

--- Lógica de programação/atividade6/ex005.js
+++ Lógica de programação/atividade6/ex005.js
@@ -2,6 +2,14 @@
 // Se o número entrado for par, retorne a mensagem "Número (Tal) é par",
 // caso contrário, retorne "Número (Tal) é ímpar";
 
-function num(prompt){
-    
+function num(){
+    const prompt = require ('prompt-sync')({sigint:true});
+    let ask = parseFloat (prompt("Digite um número qualquer "));
+    if(ask %2==0 ){
+        console.log(`O número ${ask} é par`)
+    }
+    else{
+        return `O número ${ask} é ímpar`
+    }
 }
+num();
