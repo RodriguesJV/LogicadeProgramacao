ac4c323efd3465ecaedff8d45816a1ad52e979bc
João Vitor Rodrigues <155745191+RodriguesJV@users.noreply.github.com>
2025-06-05 21:43:53 -0300

    Delete atividade6 directory

--- atividade6/ex002.js
+++
@@ -1,7 +0,0 @@
-//2. Crie uma função que dadas as dimensões de um paralelogramo
-// retorne o seu perímetro
-function periParalelogramo(a,b){
-    return 2*(a,b)
-}
-
-console.log(periParalelogramo(12,16));
--- atividade6/ex004.js
+++
@@ -1,7 +0,0 @@
-//4. Crie uma função que retorne a mensagem "A vida é bela!"
-
-function mensagem(){
-    return "A vida é bela!";
-}
-
-console.log(mensagem());
